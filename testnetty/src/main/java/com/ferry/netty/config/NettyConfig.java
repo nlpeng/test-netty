76346bc95513dd93bd190a4f1e4cf952fa9d2b05
nlpeng <nlpeng@outlook.com>
2018-12-25 20:27:12 +0800

    1.0 v test netty

---
+++ testnetty/src/main/java/com/ferry/netty/config/NettyConfig.java
@@ -0,0 +1,21 @@
+package com.ferry.netty.config;
+
+
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+/**
+ * Netty config 文件 存储整个工程的全局配置
+
+ * @author Ferry NLP
+ * @create 2018-12-25
+ * @since 1.0v
+ **/
+public class NettyConfig {
+
+    /**
+     * 存储每一个客户端接入进来时的channel对象
+     */
+    public static ChannelGroup group = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+}
